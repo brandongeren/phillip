c45206910dd3dd5ac979603577d624ebd10a70ee
Tanner <hey@tannermarino.com>
2018-04-22 17:23:08 -0500

    create installer file

---
+++ installer.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+apt-get install git
+apt-get install cmake
+apt-get install python3
+
+
+# The following snippet is from: https://github.com/p4lang/behavioral-model/blob/master/travis/install-nanomsg.sh
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $THIS_DIR/common.sh
+
+# nanomsg is very confusing in how it manages SOVERSION vs VERSION, but this
+# should be okay... (5.0.0 is the SOVERSION)
+check_lib libnanomsg libnanomsg.so.5.0.0
+
+set -e
+wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz -O nanomsg-1.0.0.tar.gz
+tar -xzvf nanomsg-1.0.0.tar.gz
+cd nanomsg-1.0.0
+mkdir build
+cd build
+# I added -DCMAKE_INSTALL_PREFIX=/usr because on my Ubuntu 14.04 machine, the
+# library is installed in /usr/local/lib/x86_64-linux-gnu/ by default, and for
+# some reason ldconfig cannot find it
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build .
+# remove sudo if you don't have permissions
+sudo cmake --build . --target install
+cd ..
+
+# The following snippet is from: https://github.com/p4lang/behavioral-model/blob/master/travis/install-nnpy.sh
+set -e
+git clone https://github.com/nanomsg/nnpy.git
+# top of tree won't install
+cd nnpy
+git checkout c7e718a5173447c85182dc45f99e2abcf9cd4065
+# remove sudo if you don't have permissions
+sudo pip install cffi
+sudo pip install .
+cd ..
+
+
+pip3 install melee
+pip3 install numpy
+pip3 install argparse
+pip3 install iproute2
+pip3 install netifacesp
+pip3 install resource
+
+git clone https://github.com/vladfi1/dolphin.git
+
+git clone https://github.com/vladfi1/phillip.git
+pip3 install -e phillip
+
+wget https://download.loveroms.com/downloader/rom/153590/1/Super%20Smash%20Bros.%20Melee%20%28USA)%20%28En,Ja)%20%28v1.02).7z?token=1524435296-5QBcNWHcQ0WtuuaywAtkzdUpDpNCxa5%2BwoBOT8ZtEyw%3D
