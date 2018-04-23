3057fc4244bb1bcd99bd1c05e963e6e7f51dbdb8
Tanner <tannn@users.noreply.github.com>
2018-04-22 20:43:18 -0500

    Install tensorflow

--- installer.sh
+++ installer.sh
@@ -44,6 +44,7 @@ sudo pip3 install melee
 sudo pip3 install numpy
 sudo pip3 install argparse
 sudo pip3 install iproute2
+sudo pip3 install tensorflow
 sudo pip3 install netifacesp
 sudo pip3 install resource
 
