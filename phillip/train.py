803f976fea1b8a820912a61ed89b856c8e624d28
Brandon Geren <brandongeren@gmail.com>
2018-04-23 16:39:44 -0500

    Hardcode the new IP address

--- phillip/train.py
+++ phillip/train.py
@@ -69,7 +69,7 @@ class Trainer(Default):
 
     # addresses = netifaces.ifaddresses(self.dump)
     # address = addresses[netifaces.AF_INET][0]['addr']
-    address = '10.137.9.13'
+    address = '10.137.41.18'
 
     util.makedirs(self.model.path)
     with open(os.path.join(self.model.path, 'ip'), 'w') as f:
